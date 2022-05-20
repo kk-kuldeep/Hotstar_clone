e315d67aa3efabc6eb505a8b169393139d7d204b
kk-kuldeep <ks13bro@gmail.com>
2022-05-20 21:42:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+const myStyle ={
+  color:"blue"
+}
+
 
 function App() {
+  const[count,setCount]=useState(0);
+  const elements= ["a","b","x","d","e","f"];
+  const name = "kuldeep";
+  let like = 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     
+      <h1 style={myStyle}> {count} {name} </h1>
+
+      <ul>
+        {
+          elements.map((value,index)=>{
+            return<li>{value}</li>
+          }
+          )
+        }
+      </ul>
+      <button onClick={()=>{
+          setCount(count+1)
+      
+      }} > Click On Me</button>
+     
     </div>
   );
 }
