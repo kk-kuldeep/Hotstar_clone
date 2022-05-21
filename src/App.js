d3b71175fa9461671b4954abb0305792a34652d2
kk-kuldeep <ks13bro@gmail.com>
2022-05-21 14:25:07 +0530

    Header Done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import React, { useState } from "react";
-const myStyle ={
-  color:"blue"
-}
-
-
+import './index.css'
+import Header from "./header";
 function App() {
-  const[count,setCount]=useState(0);
-  const elements= ["a","b","x","d","e","f"];
-  const name = "kuldeep";
-  let like = 1;
+ 
   return (
-    <div >
-     
-      <h1 style={myStyle}> {count} {name} </h1>
-
-      <ul>
-        {
-          elements.map((value,index)=>{
-            return<li>{value}</li>
-          }
-          )
-        }
-      </ul>
-      <button onClick={()=>{
-          setCount(count+1)
-      
-      }} > Click On Me</button>
-     
-    </div>
+    <Header/>
   );
 }
 
