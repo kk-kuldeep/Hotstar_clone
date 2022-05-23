327f6f53974d748f703f63ad3dce67fc05290e7e
kk-kuldeep <ks13bro@gmail.com>
2022-05-23 12:36:57 +0530

    last rem

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,27 @@ import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Viewers from './Viewers'
 import Movies from './Movies'
-
+import db from '../firebase'
+import {useDispatch} from "react-redux"
+import { setMovies } from '../features/movie/movieSlice'
 function Home() {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        console.log("hii");
+        db.collection("movies").onSnapshot((snapshot)=>{
+            let tempMovies = snapshot.docs.map((doc)=>{
+                return {id:doc.id,...doc.data()}
+            })
+            dispatch(setMovies(tempMovies));
+        });
+    },[]);
     return (
         <Container>
             <ImgSlider />
             <Viewers />
             <Movies />
         </Container>
-    )
+    );
 }
 
 export default Home
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
-import firebase from "./firebase";
-
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/storage';
 const firebaseConfig = {
   apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
   authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
@@ -14,7 +16,7 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebase.storage();
+//const storage = firebase.storage();
 
-export { auth, provider, storage };
+export { auth, provider};
 export default db;
